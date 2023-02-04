cde22ff76846daf227ca9e3c656170be5351e432
Volodymyr Petrenko <volodymyrpetrenko0@gmail.com>
2023-02-04 22:17:00 +0200

    task_5

---
+++ js/task-05.js
@@ -0,0 +1,11 @@
+const onOutput = document.getElementById("name-output");
+const onInput = document.getElementById("name-input");
+
+
+onInput.addEventListener("input", (event)=> {
+    onOutput.textContent=onInput.value;
+  });
+
+
+
+  console.log(onInput.value);
