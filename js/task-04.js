b93dca8e20b6727f0f5cb15ae74780065ea19057
Volodymyr Petrenko <volodymyrpetrenko0@gmail.com>
2023-02-04 21:37:57 +0200

    task_4_fin

--- js/task-04.js
+++ js/task-04.js
@@ -3,9 +3,11 @@ const incrEl = document.querySelector('[data-action="increment"]');
 const result = document.getElementById("value");
 
 incrEl.addEventListener("click", () => {
-    result.textContent=result.textContent+1;
+    result.textContent=Number(result.textContent)+1;
   })
 
 decrEl.addEventListener("click", () => {
     result.textContent=Number(result.textContent)-1;
   })
+
+//   Done
