f9f67d1c1afd37bdddec0337aa1a20a784066c3a
Volodymyr Petrenko <volodymyrpetrenko0@gmail.com>
2023-02-05 11:21:10 +0200

    task-8

--- js/task-07.js
+++ js/task-07.js
@@ -1,13 +1,7 @@
 const onOutput = document.getElementById("text");
 const onInput = document.getElementById("font-size-control");
 
-
 onInput.addEventListener("input", (event)=> {
-
     onOutput.style.fontSize = `${onInput.value}px`;
-
-    // onOutput.style.fontSize = `${onInput.value*2}%`;
-
-
   });
 
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+let toBackend = new Object();
 
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", onSubmitBnt);   
+
+function onSubmitBnt (event) {
+    let emailField = form.elements.email.value;
+    let passwordField = form.elements.password.value;
+
+    event.preventDefault();
+
+    if (passwordField===""||emailField===""){
+        alert("все поля должны быть заполнены");
+        // return
+    } else {
+        toBackend.email = emailField;
+        toBackend.password = passwordField;
+        console.log(toBackend);
+    };  
+
+
+form.reset();
+}
